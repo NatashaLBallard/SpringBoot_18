62f6e72e87e3595a219aa01a4eb9f8c9e178786e
natashalballard <nbitcareer@gmail.com>
2018-02-12 10:04:47 -0500

    Initial commit

---
+++ src/main/java/com/springboot_18/demo/SpringBoot18Application.java
@@ -0,0 +1,12 @@
+package com.springboot_18.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot18Application.class, args);
+	}
+}
